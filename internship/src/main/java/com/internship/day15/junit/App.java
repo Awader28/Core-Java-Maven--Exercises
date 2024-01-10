a42445b981050fff8d93f36c45303e5465c772ea
Atharva Wader <awader28@gmail.com>
2024-01-10 10:44:47 +0530

    first commit

---
+++ internship/src/main/java/com/internship/day15/junit/App.java
@@ -0,0 +1,13 @@
+package com.internship.day15.junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
